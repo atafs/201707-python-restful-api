8403a08305a20e77f50f5b755ae7b015d3a86fa0
Américo Tomás <atafs@users.noreply.github.com>
2017-07-09 17:40:22 +0100

    Project watson API in python using RESTful app

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # 201707-python-restful-api
-A simple RESTfull API that takes and stores comments identified by an SKU. 
+A simple RESTfull API that takes and stores comments identified by an SKU.
+
+# Specs and Description:
+The API should have a way in which the comment has been analysed to tell us whether the comment is positive or not. To do this, you'll use the Watson Tone Analyser for emotional tone. https://watson-api-explorer.mybluemix.net/apis/tone-analyzer-v3#!/tone/GetTone
+
+### Test Run in the web browser (changing the string(the message that is going to be analysed))
+- http://localhost:8080/watson-api/I am happy in London
+
+### Test Third party service API (curl)
+- curl -v -H "Accept: application/json" 'https://watson-api-explorer.mybluemix.net/tone-analyzer/api/v3/tone?text=I%20am%20very%20happy%20to%20be%20in%20London&tones=emotion&sentences=true&version=2017-07-01'
+
+# Additional Information:
+### Set up Python3 (linux ubuntu) and development envirnment:
+- sudo apt-get update
+- sudo apt-get -y upgrade
+- python3 -V
+- sudo apt-get install -y python3-pip
+- alias python=python3
+- sudo pip install <module>
+
+### Done:
+- understand better the watson-api-explorer
+- make a curl/postman request with success in my terminal
+- make a https request in python3
+- save the data in a json (read and write)
+- loop the dictionary for the data I need
+- create the config file
+- create the logic (scale...) to get the positive or negatice result
+
+### Todo:
+- answer to the question in the specs (the important part of the test)
+- research unit tests
+
+### Improvements:
---
+++ api/restful.py
@@ -0,0 +1,74 @@
+from bottle import run, get, post, request, delete
+import simplejson as json
+import requests
+import yaml
+import logging
+
+# functions
+def config_logger():
+	logging.basicConfig(filename='log/logger.log',level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+def read_from_config():
+	with open('config/config.yml', 'r') as ymlfile:
+	     cfg = yaml.load(ymlfile)
+
+	logging.debug('read_from_config')
+	return cfg
+
+def write_to_json(text):
+	data = json.loads(text)
+	with open('data/data.json', 'w') as jsonfile:
+	     json.dump(data['document_tone'], jsonfile)
+
+	logging.debug('write_to_json')
+
+def request_https(cfg, text):
+	url = cfg['url'] + text
+
+	logging.debug('request_https url [%s]', url)
+	return requests.get(url)
+
+def analyse_positive_comment(data):
+	data_tones = data["document_tone"]["tone_categories"][0]['tones']
+	for array in data_tones:
+	    if array['tone_name'] == 'Joy' and array['score'] > 0.5 :
+			logging.debug('analyse_positive_comment: it is a positive comment!!!')
+			return True
+
+	logging.debug('analyse_positive_comment: it is a negative comment...')
+	return False
+
+def get_message(is_analyses_positive):
+	# change color to blue or red (True or False respectivaly)
+	if (is_analyses_positive == True):
+		message = "<h3 style='color: blue'>Is it a positive comment: [%s]</h3>" %is_analyses_positive
+	else:
+		message = "<h3 style='color: red'>Is it a positive comment: [%s]</h3>" %is_analyses_positive
+
+	logging.debug(message)
+	return message
+
+
+# restful api
+@get('/watson-api/<text>')
+def getText(text):
+	# config logger
+	config_logger()
+	logging.debug('message to be analysed: [%s]\n', text)
+
+	# open the config file
+	cfg = read_from_config()
+
+	# make the https request and get a response in a string
+	response_text = request_https(cfg, text.replace(" ", "%20")).text
+
+	# writing json data
+	write_to_json(response_text)
+
+	# analyse if a comment is positive or negative and get a messaege to print on screen
+	is_analyses_positive =  analyse_positive_comment(json.loads(response_text))
+	message = get_message(is_analyses_positive)
+
+	return {message}
+
+run(reloader=True, debug=True)
